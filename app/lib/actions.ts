08ffc393df6277d5d5b9caf648641aab4b9a35b8
antsorin <antsorin@yahoo.com>
2023-11-06 16:27:40 +0200

    ch12 bugfix id in updateInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,9 +25,9 @@ export async function createInvoice(formData: FormData) {
     const date = new Date().toISOString().split('T')[0];
 
     await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -35,6 +35,7 @@ export async function createInvoice(formData: FormData) {
 
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
+        id,
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
@@ -43,10 +44,10 @@ export async function updateInvoice(id: string, formData: FormData) {
     const amountInCents = amount * 100;
 
     await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+                UPDATE invoices
+                SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+                WHERE id = ${id}
+              `;
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
